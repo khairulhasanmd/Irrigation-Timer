eb99f6f2defe1d6c061bd7f82bd3fd99d7e1304b
Md. Khairul Hasan <khairulhasanmd@gmail.com>
2019-11-04 13:58:23 +0600

    some small bug fix and debounce added

--- main.c
+++ main.c
@@ -21,7 +21,8 @@
 #define RESET_ALL PD2
 
 volatile uint8_t secondFlag;
-uint8_t gearState = 0;
+uint8_t upState = 0, dwnState = 0;//for simple debounce purpose
+uint8_t gearCount = 0;
 uint8_t valueRead = 0;
 uint8_t buttonPressed = 0; 
 uint8_t states_of_segment[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71}; //abcdefg
@@ -29,6 +30,13 @@ uint8_t states_of_segment[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
 ISR(TIMER1_COMPA_vect)
 {
   secondFlag = 1;
+}
+
+void update_display()
+{
+  //display the values
+  PORTB &= 0b10000000;
+  PORTB |= states_of_segment[gearCount];
 }
 
 int main (void)
@@ -48,40 +56,48 @@ int main (void)
   valueRead = eeprom_read_byte((uint8_t*)EEPROM_STORAGE_ADDRESS);//read eeprom
   if (valueRead == 0xFFFF)//initial value.. omit it 
   {
-    gearState = 0;
+    gearCount = 0;
   }else{//has some value and its larger 
-    gearState = valueRead;
+    gearCount = valueRead;
   }
+
   sei();//Enable Global Interrupts
 
   while (1)//main loop 
   {
     if (secondFlag){
       secondFlag = 0;
-      PORTB &= 0b10000000;
-      PORTB |= states_of_segment[gearState];
+      update_display();
       PORTB ^= (1 << SEGMENT_DOT);    // toggles the led
     }
 
     //hall effect sensor functionality
-    if (!(PIND & (1 << GEAR_SHIFT_UP))){  //if shift up detected
-      gearState ++;
-      if(gearState > 15){
-        gearState = 0;
+    if ((PIND & (1 << GEAR_SHIFT_UP)) && upState == 1){//reset up debounce
+      upState = 0;
+    }else if ((PIND & (1 << GEAR_SHIFT_DOWN)) && dwnState == 1){ //reset down debounce
+      dwnState = 0;
+    } else if ((!(PIND & (1 << GEAR_SHIFT_UP))) && upState == 0){  //if shift up detected
+      gearCount ++;
+      if(gearCount > 15){
+        gearCount = 0;
       }
       PORTB |= (1 << SEGMENT_DOT);    // light up the dot marker
-    }else if (!(PIND & (1 << GEAR_SHIFT_DOWN))){  //if shift down detected
-      gearState --;
-      if(gearState < 0){
-        gearState = 0;
+      upState = 1;
+      update_display();
+    }else if ((!(PIND & (1 << GEAR_SHIFT_DOWN))) && dwnState == 0){  //if shift down detected
+      gearCount --;
+      if(gearCount < 0){
+        gearCount = 0;
       }
       PORTB |= (1 << SEGMENT_DOT);    // light up the dot marker
+      dwnState = 1;
+      update_display();
     }else if (!(PIND & (1 << RESET_ALL))){  //if reset button pressed
       //reset the gear state stored in eeprom if it has any previous value
       cli();//Disable Global Interrupts
       valueRead = eeprom_read_byte((uint8_t*)EEPROM_STORAGE_ADDRESS);//read eeprom
       eeprom_write_byte((uint8_t*)EEPROM_STORAGE_ADDRESS,0);//write eeprom
-      gearState = 0;
+      gearCount = 0;
       PORTB |= (1 << SEGMENT_DOT);    // light up the dot marker
       _delay_ms(1000);
       sei();//Enable Global Interrupts
@@ -90,7 +106,7 @@ int main (void)
     //-------------------------------POWER DOWN, SAVE IMMEDIATELY TO EEPROM
     if (!(PIND & (1 << POWER_OFF_DETECT))){ //active low
       cli();//Disable Global Interrupts
-      eeprom_write_byte((uint8_t*)EEPROM_STORAGE_ADDRESS, gearState);//write eeprom
+      eeprom_write_byte((uint8_t*)EEPROM_STORAGE_ADDRESS, gearCount);//write eeprom
       PORTB |= (1 << SEGMENT_DOT);    // light up the dot marker
       _delay_ms(500); //delay till the capacitor discharge
       sei();//Enable Global Interrupts
