e2eebbc3f03641fa7201494c9d9799de0e0c78f4
Md. Khairul Hasan <khairulhasanmd@gmail.com>
2019-12-15 15:52:15 +0600

    atleast rotary encoder working with 7segment

--- main.c
+++ main.c
@@ -10,43 +10,77 @@
 #include <avr/eeprom.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#define F_CPU 1000000UL
 #include <util/delay.h>
 
-#define F_CPU 1000000UL
+
 #define SEGMENT_SCAN_DELAY 5
+// #define ENCODER_CLK 2       //clk
+// #define ENCODER_DATA 3       //dt
+// #define ENCODER_SWITCH 4  //push button switch
 #define EEPROM_STORAGE_ADDRESS 0x08
-#define POWER_OFF_DETECT PD4
 #define SEGMENT_DOT PB7
-#define GEAR_SHIFT_UP PD0
-#define GEAR_SHIFT_DOWN PD1
-#define RESET_ALL PD2
+#define POWER_OFF_DETECT PD1
+#define CLK PD2
+#define DAT PD3
+#define BTN PD4
+//modes
+#define HOUR_SET 1
+#define MINUTE_SET 2
+#define SECOND_SET 3
+#define RUN 4
 
 volatile uint8_t secondFlag;
-uint8_t upState = 0, dwnState = 0;//for simple debounce purpose
-uint8_t gearCount = 0;
-uint8_t valueRead = 0;
-uint8_t buttonPressed = 0; 
+uint8_t buttonPressed = 0, remHr = 0, remMin = 0, remSec = 0, MODE = 0; 
 uint8_t states_of_segment[16] = {~0x3F, ~0x06, ~0x5B, ~0x4F, ~0x66, ~0x6D, ~0x7D, ~0x07, ~0x7F, ~0x6F, ~0x77, ~0x7C, ~0x39, ~0x5E, ~0x79, ~0x71}; //abcdefg
 uint8_t segment[4] = {1,2,3,4};
+int MSB, LSB, encoded, sum;
+int lastEncoded = 0, encoderValue = 0;
 
-ISR(TIMER1_COMPA_vect)
-{
-  secondFlag = 1;
+void updateEncoder(void){
+  MSB = 0;
+  LSB = 0;
+  if(PIND & (1 << PD2)){
+    MSB = 1;
+  }
+  if(PIND & (1 << PD3)){
+    LSB = 1;
+  }
+  encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number 
+  sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value 
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+  lastEncoded = encoded; //store this value for next time 
 }
 
-void update_display()
-{
+void update_display(void){
   //display the values
+  if(encoderValue > 1){
+    remSec++;
+    encoderValue = 0;
+  }else if(encoderValue < -1){
+    remSec--;
+    encoderValue = 0;
+  }
+  // remSec = encoderValue;
+  segment[3] = remSec%10;
+  segment[2] = remSec/10;
+  segment[1] = remSec%10;
+  segment[0] = remSec/10;
+
+
   PORTB = 0x00;
   PORTC &= 0b00000011; //common anode
   PORTB |= (states_of_segment[segment[3]]);
   PORTC |= 0b00100000; //common anode
   _delay_ms(SEGMENT_SCAN_DELAY);
+
   PORTB = 0x00;
   PORTC &= 0b00000011; //common anode
   PORTB |= (states_of_segment[segment[2]]);
   PORTC |= 0b00010000; //common anode
   _delay_ms(SEGMENT_SCAN_DELAY);
+
   PORTB = 0x00;
   PORTC &= 0b00000011; //common anode
   PORTB |= (states_of_segment[segment[1]]);
@@ -59,27 +93,36 @@ void update_display()
   _delay_ms(SEGMENT_SCAN_DELAY);
 }
 
-// void update_display()
-// {
-//   //display the values
-//   PORTB &= 0b01111111;
-//   // PORTB &= 0b10000000;
-//   PORTB |= ~(states_of_segment[gearCount]);
-// }
+ISR(TIMER1_COMPA_vect)
+{
+  secondFlag = 1;
+}
+
+ISR (INT0_vect)
+{
+  updateEncoder();
+}
+
+ISR (INT1_vect)
+{
+  updateEncoder();
+}
 
 int main (void)
 {
   //initialize the I/O Ports
+  DDRD &= ~((1 << CLK)|(1 << DAT)|(1 << BTN)|(1 << POWER_OFF_DETECT));    // switch on pin (GEAR_SHIFT_UP)
+  PORTD |= (1 << CLK)|(1 << DAT)|(1 << BTN)|(1 << POWER_OFF_DETECT);    // enable pull-up resistor
   DDRC |= (1 << PC5)|(1 << PC4)|(1 << PC3)|(1 << PC2); //common anode
   DDRB |= (1 << PB0)|(1 << PB1)|(1 << PB2)|(1 << PB3)|(1 << PB4)|(1 << PB5)|(1 << PB6)|(1 << SEGMENT_DOT); //seven segment gbcdefa.
-  DDRD &= ~((1 << GEAR_SHIFT_UP)|(1 << GEAR_SHIFT_DOWN)|(1 << RESET_ALL)|(1 << POWER_OFF_DETECT));    // switch on pin (GEAR_SHIFT_UP)
-  PORTD |= (1 << GEAR_SHIFT_UP)|(1 << GEAR_SHIFT_DOWN)|(1 << RESET_ALL)|(1 << POWER_OFF_DETECT);    // enable pull-up resistor
 
   TCCR1B |= (1 << WGM12);           // Configure timer 1 for CTC mode
   TIMSK |= (1 << OCIE1A);           // Enable CTC interrupt
   OCR1A = 15624;                // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
   TCCR1B |= ((1 << CS10) | (1 << CS11));  // Start timer at Fcpu/64
 
+
+  remSec = 99;
   //READ FROM EEPROM
   // valueRead = eeprom_read_byte((uint8_t*)EEPROM_STORAGE_ADDRESS);//read eeprom
   // if (valueRead > 0x0F)//initial value.. omit it 
@@ -88,15 +131,16 @@ int main (void)
   // }else{//has some value and its larger 
   //   gearCount = valueRead;
   // }
-  
+  MCUCR |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
+  GICR |= ((1 << INT0)|(1 << INT1));      // Turns on INT0 & int1
+
   sei();//Enable Global Interrupts
 
   while (1)//main loop 
   {
     if (secondFlag){
-      // secondFlag = 0;
-      gearCount = 1;
-      
+      secondFlag = 0;
+      // remSec--;
       // PORTB ^= (1 << SEGMENT_DOT);    // toggles the led
     }
     update_display();
