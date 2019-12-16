afcb55a8010ca6d35c66ebefb80b07e43f186bf1
Md. Khairul Hasan <khairulhasanmd@gmail.com>
2019-12-16 14:10:11 +0600

    cleanup and now the timer functioning almost

--- main.c
+++ main.c
@@ -16,22 +16,26 @@
 #define SEGMENT_SCAN_DELAY 5
 #define SEGMENT_TOTAL_TIME 30
 #define SEGMENT_BLINK_DELAY 15
-#define EEPROM_STORAGE_ADDRESS 0x08
+
+#define EEPROM_SECOND_SET_ADDRESS 0x00
+#define EEPROM_MINUTE_SET_ADDRESS 0x01
+#define EEPROM_HOUR_SET_ADDRESS 0x02
+
 #define SEGMENT_DOT PB7
 #define POWER_OFF_DETECT PD1
 #define CLK PD2
 #define DAT PD3
 #define BTN PD4
 //modes
-#define HOUR_SET 1
+#define RUN 0
+#define SECOND_SET 1
 #define MINUTE_SET 2
-#define SECOND_SET 3
-#define RUN 4
+#define HOUR_SET 3
 
 volatile uint8_t secondFlag;
 uint8_t buttonPressed = 0, curHr = 0, curMin = 0, curSec = 0, STATE = 0; 
 uint8_t states_of_segment[16] = {~0x3F, ~0x06, ~0x5B, ~0x4F, ~0x66, ~0x6D, ~0x7D, ~0x07, ~0x7F, ~0x6F, ~0x77, ~0x7C, ~0x39, ~0x5E, ~0x79, ~0x71}; //abcdefg
-uint8_t segment[4] = {1,2,3,4};
+uint8_t segment[4] = {0,0,0,0};
 int MSB, LSB, encoded, sum;
 int lastEncoded = 0, encoderValue = 0, segmentBlink = 0;
 
@@ -48,7 +52,7 @@ void updateEncoder(void){
   if(PIND & (1 << PD2)){
     MSB = 1;
   }
-  if(PIND & (1 << PD3)){`
+  if(PIND & (1 << PD3)){
     LSB = 1;
   }
   encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number 
@@ -63,8 +67,8 @@ void update_display(void){
   segment[3] = curSec%10;
   segment[2] = curSec/10;
   //first 8800 segment
-  segment[1] = curSec%10;
-  segment[0] = curSec/10;
+  segment[1] = curMin%10;
+  segment[0] = curMin/10;
   PORTB &= 0x80;
   PORTC &= 0b00000011; //common anode
   PORTB |= (states_of_segment[segment[3]]);
@@ -109,13 +113,9 @@ void blink_display_last2digit(void){
   }else if(segmentBlink > SEGMENT_BLINK_DELAY){//off time
     PORTB &= 0x80;
     PORTC &= 0b00000011; //common anode
-    // PORTB |= (states_of_segment[segment[3]]);
-    // PORTC |= 0b00100000; //common anode
     _delay_ms(SEGMENT_SCAN_DELAY);
     PORTB &= 0x80;
     PORTC &= 0b00000011; //common anode
-    // PORTB |= (states_of_segment[segment[2]]);
-    // PORTC |= 0b00010000; //common anode
     _delay_ms(SEGMENT_SCAN_DELAY);
   }
   PORTB &= 0x80;
@@ -136,8 +136,8 @@ void blink_display_first2digit(void){
     segmentBlink = 0;
   }
   //first 8800 segment
-  segment[1] = curSec%10;
-  segment[0] = curSec/10;
+  segment[1] = curMin%10;
+  segment[0] = curMin/10;
   PORTB &= 0x80;
   PORTC &= 0b00000011; //common anode
   PORTB |= (states_of_segment[segment[3]]);
@@ -197,66 +197,48 @@ int main (void)
   OCR1A = 15624;                // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
   TCCR1B |= ((1 << CS10) | (1 << CS11));  // Start timer at Fcpu/64
 
-  curSec = 99;
-  STATE = SECOND_SET;
+  STATE = RUN;
   //READ FROM EEPROM
-  // valueRead = eeprom_read_byte((uint8_t*)EEPROM_STORAGE_ADDRESS);//read eeprom
-  // if (valueRead > 0x0F)//initial value.. omit it 
-  // {
-  //   gearCount = 0;
-  // }else{//has some value and its larger 
-  //   gearCount = valueRead;
-  // }
+  curSec = eeprom_read_byte((uint8_t*)EEPROM_SECOND_SET_ADDRESS);//read eeprom
+  curMin = eeprom_read_byte((uint8_t*)EEPROM_MINUTE_SET_ADDRESS);//read eeprom
+  curHr = eeprom_read_byte((uint8_t*)EEPROM_HOUR_SET_ADDRESS);//read eeprom
+  if (curSec > 99) curSec = 0;//initial value.. omit it 
+  if (curMin > 99) curMin = 0;//initial value.. omit it 
+  if (curHr > 99) curHr = 0;//initial value.. omit it 
+
+  //interrupt
   MCUCR |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
   GICR |= ((1 << INT0)|(1 << INT1));      // Turns on INT0 & int1
-
   sei();//Enable Global Interrupts
 
-  while (1)//main loop 
+  while (1)//main loop ---------------------------------------------------------------------------------------
   {
+    if(!(PIND & (1 << BTN))){//button pressed
+      STATE++;
+      if (STATE > MINUTE_SET){ //FIX IT WHEN HOUR ENABLED
+        STATE = RUN;
+        eeprom_write_byte((uint8_t*)EEPROM_SECOND_SET_ADDRESS, curSec);//write eeprom
+        eeprom_write_byte((uint8_t*)EEPROM_MINUTE_SET_ADDRESS, curMin);//write eeprom
+        eeprom_write_byte((uint8_t*)EEPROM_HOUR_SET_ADDRESS, curHr);//write eeprom
+      }
+      waitForRelease();
+    }
 
-    // if(digitalRead(BTN) == LOW){
-        // if (STATE == SETUP){
-            // encoderValue = 0;
-            // if (menu_level == 0 && menu_index == 0){//RUN
-            //     waitForRelease();
-            //     print_menu();
-            //     menu_level = 0;
-            //     STATE = RUN;
-            // }else if (menu_level == 0 && menu_index < menu_count){//submenu selected
-            //     menu_level = 1;
-            //     waitForRelease();
-            //     print_sub_menu();
-            // }else if (menu_level == 0 && menu_index == menu_count){//save menu selected
-            //     menu_index = 0;
-            //     save();
-            //     waitForRelease();
-            //     print_menu();
-            // }else if (menu_level == 1 && sub_menu_index == sub_menu_count){//selected exit on sub menu
-            //     menu_level = 0;
-            //     sub_menu_index = 0;
-            //     waitForRelease();
-            //     print_menu();
-            // }else if (menu_level == 1 && sub_menu_index < sub_menu_count){//set time param hms
-            //     menu_level = 2;
-            //     waitForRelease();
-            //     print_time_menu();
-            // }else if (menu_level == 2 && sub_menu_index < sub_menu_count){//selected exit on time menu
-            //     menu_level = 1;
-            //     sub_menu_index = 0;
-            //     waitForRelease();
-            //     print_sub_menu();
-            // }
-        // }else if (STATE == RUN){
-            // waitForRelease();
-            // menu_level = 0;
-            // menu_index = 0;
-            // STATE = SETUP;
-            // print_run_menu();
-        // }
-    // }
-
-    if (STATE == SECOND_SET){
+    if (STATE == RUN){
+      if (secondFlag){
+        secondFlag = 0;
+        // PORTB ^= (1 << SEGMENT_DOT);    // toggles the led
+        if(curSec > 0 ){
+          curSec--;
+        }else if(curMin > 0 && curSec == 0){
+          curSec = 59;
+          curMin --;
+        }else if(curSec == 0 && curMin == 0){
+          //do anything
+        }
+      }
+      update_display();
+    } else if (STATE == SECOND_SET){
       if(encoderValue > 1){
         curSec++;
         encoderValue = 0;
@@ -266,6 +248,7 @@ int main (void)
         encoderValue = 0;
         segmentBlink = 0;
       }
+      blink_display_last2digit();
     }else if (STATE == MINUTE_SET){
       if(encoderValue > 1){
         curMin++;
@@ -274,43 +257,27 @@ int main (void)
         curMin--;
         encoderValue = 0;
       }
-    }else if (STATE == RUN){
-        if (secondFlag){
-        secondFlag = 0;
-        // curSec--;
-        // PORTB ^= (1 << SEGMENT_DOT);    // toggles the led
-      } 
-        // if(timearr[RUN_TEMP_VARIABLE][HOUR] == 0 &&
-        //                 timearr[RUN_TEMP_VARIABLE][MINUTE] == 0 && 
-        //                 timearr[RUN_TEMP_VARIABLE][SECOND] == 0){
-        //     menu_index ++;
-        //     if (menu_index > (menu_count - 1)){
-        //         menu_index = 1;//time starts from there
-        //     }
-        //     timearr[RUN_TEMP_VARIABLE][HOUR] = timearr[menu_index][HOUR];
-        //     timearr[RUN_TEMP_VARIABLE][MINUTE] = timearr[menu_index][MINUTE];
-        //     timearr[RUN_TEMP_VARIABLE][SECOND] = timearr[menu_index][SECOND];
-        // }else{
-        //     unsigned long currentMillis = millis();
-        //     if (currentMillis - previousMillis >= 1000) {
-        //         previousMillis = currentMillis;
-        //         // reduce_a_second();
-        //         // print_run_menu();
-        //     }
-        // }
+      blink_display_first2digit();
+    }else if (STATE == HOUR_SET){
+      if(encoderValue > 1){
+        curHr++;
+        encoderValue = 0;
+      }else if(encoderValue < -1){
+        curHr--;
+        encoderValue = 0;
+      }
+      blink_display_first2digit();//needs fix
     }
 
-  //   //-------------------------------POWER DOWN, SAVE IMMEDIATELY TO EEPROM
-  //   if (!(PIND & (1 << POWER_OFF_DETECT))){ //active low
-  //     cli();//Disable Global Interrupts
-  //     eeprom_write_byte((uint8_t*)EEPROM_STORAGE_ADDRESS, gearCount);//write eeprom
-  //     PORTB |= (1 << SEGMENT_DOT);    // light up the dot marker
-  //     _delay_ms(500); //delay till the capacitor discharge
-  //     sei();//Enable Global Interrupts
-  //   }
-
-    // update_display();
-    // blink_display_last2digit();
-    blink_display_first2digit();
+    //-------------------------------POWER DOWN, SAVE IMMEDIATELY TO EEPROM
+    // if (!(PIND & (1 << POWER_OFF_DETECT))){ //active low
+    //   cli();//Disable Global Interrupts
+    //   eeprom_write_byte((uint8_t*)EEPROM_SECOND_SET_ADDRESS, curSec);//write eeprom
+    //   eeprom_write_byte((uint8_t*)EEPROM_MINUTE_SET_ADDRESS, curMin);//write eeprom
+    //   eeprom_write_byte((uint8_t*)EEPROM_HOUR_SET_ADDRESS, curHr);//write eeprom
+    //   PORTB |= (1 << SEGMENT_DOT);    // light up the dot marker
+    //   _delay_ms(500); //delay till the capacitor discharge
+    //   sei();//Enable Global Interrupts
+    // }
   }
 }
